a32419310440cdf6e82b5a90d8af6ab17fa8435b
league <maddoxseul@sbsd.net>
2024-03-17 11:28:43 -0700

    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

--- src/_10_slot_machine/slotMachine.java
+++ src/_10_slot_machine/slotMachine.java
@@ -13,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class slotMachine implements ActionListener {
@@ -21,8 +22,11 @@ public class slotMachine implements ActionListener {
 	JPanel randomPanel = new JPanel();
 	JPanel buttonPanel = new JPanel();
 	JPanel moneyPanel = new JPanel();
+	JLabel money = new JLabel();
+	int moneyText = 0;
 	JButton button = new JButton("SPIN");
 	String images[] = {"cherry.png", "lime.png", "orange.png"};
+
 
 
 	public void buttons() {
@@ -34,12 +38,12 @@ public class slotMachine implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setSize(1000, 600);
+		moneyPanel.add(money);
+		money.setText("Money = "+ moneyText);
 		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 		randomPanel.setSize(new Dimension(1000/3, 600));
 		buttonPanel.setSize(new Dimension(1000/3, 600));
 		moneyPanel.setSize(new Dimension(1000/3, 600));
-
-
 		frame.pack();
 		
 	}
@@ -59,18 +63,34 @@ public class slotMachine implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource() == button) {
+		randomPanel.removeAll();
+			String newImages[] = new String[3];
 			for(int i = 0; i<3; i+=1) {
+				newImages[i] = images[ran.nextInt(3)];
+				
+			}		
+						
+			for(int j = 0; j<3; j+=1) {
 				try {
-					randomPanel.add(createLabelImage(images[ran.nextInt(3)]));
+					randomPanel.add(createLabelImage(newImages[j]));
 				} catch (MalformedURLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-				
 			}
+
+				
+			
+			if(e.getSource() == button) {
+				if(newImages[0] == (newImages[1]) & newImages[0] == (newImages[2])) {
+					moneyText += 500;
+					money.setText("money = "+ moneyText);
+					JOptionPane.showMessageDialog(null, "YOU WIN!!!");
+
+				}
+				frame.pack();
 		}
+	
 	}
 }
+
